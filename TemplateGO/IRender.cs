a0d48cc66e55927c13983f572d9f2a26bd5abbd2
yanglb <me@yanglb.com>
2022-09-27 17:50:31 +0800

    添加 IRender 接口定义

---
+++ TemplateGO/IRender.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace TemplateGO
+{
+    internal interface IRender
+    {
+        /// <summary>
+        /// 渲染模板
+        /// </summary>
+        /// <param name="templatePath">模板路径</param>
+        /// <param name="data">数据</param>
+        public void Render(string templatePath, JsonElement data);
+    }
+}
