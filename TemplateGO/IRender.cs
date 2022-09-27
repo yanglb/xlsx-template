c07aa14dd83a03cc6b73d4707e40e7c72a8f24b4
yanglb <me@yanglb.com>
2022-09-27 18:43:47 +0800

    Render 接口添加 目标文件类型 参数

--- TemplateGO/IRender.cs
+++ TemplateGO/IRender.cs
@@ -9,6 +9,7 @@ namespace TemplateGO
         /// </summary>
         /// <param name="templatePath">模板路径</param>
         /// <param name="data">数据</param>
-        public void Render(string templatePath, JsonElement data);
+        /// <param name="targetType">目标文件类型</param>
+        public void Render(string templatePath, JsonElement data, string? targetType);
     }
 }
