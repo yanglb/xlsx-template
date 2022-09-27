2030c8ca3bc70c3257a88228adfc7e8b3f588c84
yanglb <me@yanglb.com>
2022-09-27 20:12:19 +0800

    添加测试代码

---
+++ TemplateGOTests/R.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace TemplateGOTests
+{
+    internal static class R
+    {
+        private static string AppDir
+        {
+            get
+            {
+                var file = Process.GetCurrentProcess().MainModule?.FileName;
+
+                // 不存在返回当前路径
+                if (string.IsNullOrEmpty(file)) return "";
+
+                return Path.GetDirectoryName(file) ?? "";
+            }
+        }
+
+        /// <summary>
+        /// 获取相对于工程目录的文件（仅用于测试）
+        /// </summary>
+        internal static string FullPath(string relativeProjPath)
+        {
+            var path = Path.Join(AppDir, "../../..", relativeProjPath);
+            return Path.GetFullPath(path);
+        }
+    }
+}
---
+++ TemplateGOTests/TemplateGOTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text.Json;
+using TemplateGOTests;
+
+namespace TemplateGO.Tests
+{
+    [TestClass()]
+    public class TemplateGOTests
+    {
+        private JsonElement JsonFromFile(string file)
+        {
+            var fullPath = R.FullPath(file);
+            var jsonString = File.ReadAllText(fullPath);
+            return JsonDocument.Parse(jsonString)!.RootElement;
+        }
+
+        [TestMethod()]
+        public void RenderTestXltx()
+        {
+            TemplateGO.Render(R.FullPath("data/test.xlsx"), JsonFromFile("data/test.json"), "out.xlsx");
+            Assert.Fail();
+        }
+    }
+}
