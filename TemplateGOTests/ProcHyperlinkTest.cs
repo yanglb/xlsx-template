b484d1200ce386c8d8d64274e2cb92abf9c45c39
yanglb <me@yanglb.com>
2022-09-28 19:24:31 +0800

    添加超链接处理测试代码

---
+++ TemplateGOTests/ProcHyperlinkTest.cs
@@ -0,0 +1,43 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using TemplateGOTests;
+
+namespace TemplateGO.Tests
+{
+    [TestClass()]
+    public class ProcHyperlinkTest
+    {
+        [TestMethod()]
+        public void HyperlinkTest()
+        {
+            TemplateGO.Render(R.FullPath("data/hyperlink.xlsx"), R.JsonFromFile("data/hyperlink.json"), "hyperlink-out.xlsx");
+
+            // 应该能打开文档
+            using (var doc = SpreadsheetDocument.Open("hyperlink-out.xlsx", false))
+            {
+                Assert.IsNotNull(doc);
+                Assert.IsNotNull(doc.WorkbookPart);
+                var sheets = doc.WorkbookPart.Workbook.Descendants<Sheet>();
+                Assert.IsNotNull(sheets);
+
+                // 所有的连接应该为 hyperink.json 中的
+                var siPart = doc.WorkbookPart.WorksheetParts.GetEnumerator();
+                while (siPart.MoveNext())
+                {
+                    foreach (var link in siPart.Current?.HyperlinkRelationships!)
+                    {
+                        var links = siPart.Current.Worksheet.Descendants<Hyperlinks>().FirstOrDefault();
+                        var hLink = links?.Descendants<Hyperlink>().Where(r => r.Id == link.Id).FirstOrDefault();
+                        Assert.AreEqual(
+                            link.Uri.ToString(),
+                            "http://yanglb.com/from-test",
+                            $"Sheet = {siPart.Current.Worksheet.LocalName}, Cell = {hLink?.Reference} 数据不正确"
+                        );
+                    }
+                }
+            }
+        }
+    }
+}
