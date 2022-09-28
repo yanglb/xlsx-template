2dc27b90071c3afe42f5065eaf6fdb6a5b8129b4
yanglb <me@yanglb.com>
2022-09-28 08:42:15 +0800

    添加 处理器类型 定义

---
+++ TemplateGO/Processor/ProcessorType.cs
@@ -0,0 +1,33 @@
+﻿namespace TemplateGO.Processor
+{
+    /// <summary>
+    /// 处理器类型
+    /// </summary>
+    public class ProcessorType
+    {
+        /// <summary>
+        /// 值处理
+        /// </summary>
+        public const string Value = "";
+
+        /// <summary>
+        /// 超连接
+        /// </summary>
+        public const string Hyperlink = "link";
+
+        /// <summary>
+        /// 图片
+        /// </summary>
+        public const string Image = "img";
+
+        /// <summary>
+        /// 二维码
+        /// </summary>
+        public const string QRCode = "qr";
+
+        /// <summary>
+        /// 表格
+        /// </summary>
+        public const string Table = "table";
+    }
+}
