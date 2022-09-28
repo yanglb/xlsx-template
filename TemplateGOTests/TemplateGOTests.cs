d3531dc371f7ecc6dfc101f6e231dd15c6ee0999
yanglb <me@yanglb.com>
2022-09-28 19:52:20 +0800

    添加TemplateGO测试数据及改进测试代码

--- TemplateGOTests/TemplateGOTests.cs
+++ TemplateGOTests/TemplateGOTests.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using TemplateGOTests;
 
 namespace TemplateGO.Tests
@@ -7,10 +10,71 @@ namespace TemplateGO.Tests
     public class TemplateGOTests
     {
         [TestMethod()]
-        public void RenderTestXltx()
+        public void GoTestXlsx()
         {
-            TemplateGO.Render(R.FullPath("data/test.xlsx"), R.JsonFromFile("data/test.json"), "out.xlsx");
-            Assert.Fail();
+            TemplateGO.Render(R.FullPath("data/go.xlsx"), R.JsonFromFile("data/go.json"), "go-out-xlsx.xlsx");
+
+            // 应该能打开文档
+            using (var doc = SpreadsheetDocument.Open("go-out-xlsx.xlsx", false))
+            {
+                Assert.IsNotNull(doc);
+                Assert.IsNotNull(doc.WorkbookPart);
+                var sheets = doc.WorkbookPart.Workbook.Descendants<Sheet>();
+                Assert.IsNotNull(sheets);
+
+                var ssTable = doc.WorkbookPart?.SharedStringTablePart?.SharedStringTable;
+                var siPart = doc.WorkbookPart?.WorksheetParts.GetEnumerator();
+                var cellValues = new Dictionary<string, string>() {
+                    {"B3", "来自测试数据" },
+                    {"B4", "来自测试数据" },
+                    {"B5", "${hello|nono}" }
+                };
+                while (siPart != null && siPart.MoveNext())
+                {
+                    var cells = siPart.Current.Worksheet.Descendants<Cell>();
+                    foreach(var cell in cells)
+                    {
+                        if (!cellValues.ContainsKey(cell.CellReference!)) continue;
+
+                        var value = Utils.GetCellString(cell, ssTable);
+                        Assert.AreEqual(cellValues[cell.CellReference!], value);
+                    }
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void GoTestXltx()
+        {
+            TemplateGO.Render(R.FullPath("data/go.xltx"), R.JsonFromFile("data/go.json"), "go-out-xltx.xlsx");
+
+            // 应该能打开文档
+            using (var doc = SpreadsheetDocument.Open("go-out-xltx.xlsx", false))
+            {
+                Assert.IsNotNull(doc);
+                Assert.IsNotNull(doc.WorkbookPart);
+                var sheets = doc.WorkbookPart.Workbook.Descendants<Sheet>();
+                Assert.IsNotNull(sheets);
+
+                var ssTable = doc.WorkbookPart?.SharedStringTablePart?.SharedStringTable;
+                var siPart = doc.WorkbookPart?.WorksheetParts.GetEnumerator();
+                var cellValues = new Dictionary<string, string>() {
+                    {"B3", "来自测试数据" },
+                    {"B4", "来自测试数据" },
+                    {"B5", "${hello|nono}" }
+                };
+                while (siPart != null && siPart.MoveNext())
+                {
+                    var cells = siPart.Current.Worksheet.Descendants<Cell>();
+                    foreach (var cell in cells)
+                    {
+                        if (!cellValues.ContainsKey(cell.CellReference!)) continue;
+
+                        var value = Utils.GetCellString(cell, ssTable);
+                        Assert.AreEqual(cellValues[cell.CellReference!], value);
+                    }
+                }
+            }
         }
     }
 }
