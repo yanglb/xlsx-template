fab4b0140f301eed545a6e741e7e88295ed7165a
yanglb <me@yanglb.com>
2022-09-28 15:29:50 +0800

    添加用于获取处理程序的工厂方法

---
+++ TemplateGO/Processor/Processor.cs
@@ -0,0 +1,31 @@
+﻿namespace TemplateGO.Processor
+{
+    internal class Processor
+    {
+        private static Dictionary<string, Type> ProcessorTypeMap = new Dictionary<string, Type>()
+        {
+            {ProcessorType.Value, typeof(ValueProcess) },
+            {ProcessorType.Hyperlink, typeof(ValueProcess) },
+        };
+
+        /// <summary>
+        /// 获取处理程序
+        /// </summary>
+        /// <param name="procType">处理器种类</param>
+        public static IProcessor? ProcessorByType(string procType)
+        {
+            if (!ProcessorTypeMap.ContainsKey(procType))
+            {
+                Console.WriteLine($"未定义 {procType} 处理程序。");
+                return null;
+            }
+
+            var type = ProcessorTypeMap[procType];
+            if (Activator.CreateInstance(type) is not IProcessor ins)
+            {
+                throw new Exception($"无法实例化用于处理 {procType} 的处理程序。");
+            }
+            return ins;
+        }
+    }
+}
