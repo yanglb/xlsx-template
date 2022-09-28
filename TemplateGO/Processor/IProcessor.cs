3e98b2f60f400255844eb76ed6f3e77c802d54ad
yanglb <me@yanglb.com>
2022-09-28 15:29:23 +0800

    添加处理程序接口定义

---
+++ TemplateGO/Processor/IProcessor.cs
@@ -0,0 +1,18 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System.Text.Json;
+
+namespace TemplateGO.Processor
+{
+    internal interface IProcessor
+    {
+        /// <summary>
+        /// 处理模板内容
+        /// </summary>
+        /// <param name="cell">单元格</param>
+        /// <param name="originValue">单元格原始值</param>
+        /// <param name="parser">解析器</param>
+        /// <param name="sheet">所属Sheet</param>
+        /// <param name="data">数据</param>
+        internal void Process(Cell cell, string originValue, Parser parser, Sheet sheet, JsonElement data, SharedStringTable? sharedStringTable);
+    }
+}
