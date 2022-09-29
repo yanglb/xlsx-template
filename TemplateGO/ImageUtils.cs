7d656efca7676b40c6821e4be6aafea53c5bd102
yanglb <me@yanglb.com>
2022-09-29 10:38:57 +0800

    添加ImageUtils及其测试代码
    支持
    1. Base64编码内容
    2. 网络图片

---
+++ TemplateGO/ImageUtils.cs
@@ -0,0 +1,107 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TemplateGO
+{
+    public static class ImageUtils
+    {
+        /// <summary>
+        /// 获取图片文件
+        /// </summary>
+        /// <param name="image">文件、网络地址、Base64编码的内容</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static string ToLocalFile(string image)
+        {
+            if (string.IsNullOrEmpty(image)) throw new ArgumentNullException("图片地址或内容不能为空");
+
+            // Base64 编码
+            // data:[mediatype][;base64],{base64-data}
+            if (image.StartsWith("data:image")) return SaveBase64Image(image);
+
+            // 网络图片
+            if (Regex.IsMatch(image, @"^http(s)?://", RegexOptions.IgnoreCase))
+            {
+                return DownloadImageFile(image);
+            }
+
+            // 其它当作文件处理
+            return image;
+        }
+
+        /// <summary>
+        /// 将Base64编码的内容保存到文件中
+        /// </summary>
+        /// <param name="imageBase64Data">data:[mediatype][;base64],{base64-data}</param>
+        /// <returns>文件路径</returns>
+        /// <exception cref="ArgumentException">不支持的类型时</exception>
+        private static string SaveBase64Image(string imageBase64Data)
+        {
+            var idx = imageBase64Data.IndexOf(',');
+            var meta = imageBase64Data[..idx];
+            var base64 = imageBase64Data[(idx + 1)..];
+
+            var imageType = Regex.Match(meta, @"data:(image/[\w-]+)").Groups[1].Value;
+            if (string.IsNullOrEmpty(imageType)) throw new ArgumentException("不是有效的Base64编码图片。");
+            var filePath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + GetImageExtension(imageType));
+
+            using (var fs = File.OpenWrite(filePath))
+            {
+                var data = Convert.FromBase64String(base64);
+                fs.Write(data);
+            }
+            return filePath;
+        }
+
+        private static string DownloadImageFile(string imageUrl)
+        {
+            var task = DownloadImageFileAsync(imageUrl);
+            task.Wait();
+            return task.Result;
+        }
+
+        private static async Task<string> DownloadImageFileAsync(string imageUrl)
+        {
+            var client = new HttpClient();
+            var res = await client.GetAsync(imageUrl);
+            res.EnsureSuccessStatusCode();
+
+            // 最终文件路径
+            string filePath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName() + "-" + Path.GetFileName(imageUrl));
+
+            using var fs = File.Open(filePath, FileMode.Create);
+            var st = await res.Content.ReadAsByteArrayAsync();
+            fs.Write(st);
+            return filePath;
+        }
+
+        private static string GetImageExtension(string imageType)
+        {
+            switch (imageType)
+            {
+                case "image/bmp":
+                    return ".bmp";
+                case "image/gif":
+                    return ".gif";
+                case "image/png":
+                    return ".png";
+                case "image/tiff":
+                    return ".tiff";
+                case "image/x-icon":
+                    return ".icon";
+                case "image/x-pcx":
+                    return ".pcx";
+                case "image/jpeg":
+                    return ".jpeg";
+                case "image/x-emf":
+                    return ".emf";
+                case "image/x-wmf":
+                    return ".wmf";
+                case "image/svg+xml":
+                    return ".svg";
+                default:
+                    throw new ArgumentOutOfRangeException("imageType");
+            }
+        }
+
+    }
+}
---
+++ TemplateGOTests/ImageUtilsTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Text;
+
+namespace TemplateGO.Tests
+{
+    [TestClass()]
+    public class ImageUtilsTests
+    {
+        [TestMethod()]
+        public void LocalFileTest()
+        {
+            var file = ImageUtils.ToLocalFile("local.png");
+            Assert.AreEqual("local.png", file);
+        }
+
+        [TestMethod()]
+        public void Base64Test()
+        {
+            var data = Encoding.UTF8.GetBytes("hello");
+            var base64Data = $"data:image/png;base64,{Convert.ToBase64String(data)}";
+
+            var file = ImageUtils.ToLocalFile(base64Data);
+            Assert.IsTrue(file.EndsWith(".png"));
+            Assert.IsTrue(File.Exists(file));
+
+            var fileData = Encoding.UTF8.GetString(File.ReadAllBytes(file));
+            Assert.AreEqual("hello", fileData);
+        }
+
+        [TestMethod()]
+        public void Base64Test2()
+        {
+            var data = Encoding.UTF8.GetBytes("hello");
+            var base64Data = $"data:image/x-pcx,{Convert.ToBase64String(data)}";
+
+            var file = ImageUtils.ToLocalFile(base64Data);
+            Assert.IsTrue(file.EndsWith(".pcx"));
+            Assert.IsTrue(File.Exists(file));
+
+            var fileData = Encoding.UTF8.GetString(File.ReadAllBytes(file));
+            Assert.AreEqual("hello", fileData);
+        }
+
+        [TestMethod()]
+        public void Base64TestBad()
+        {
+            var data = Encoding.UTF8.GetBytes("hello");
+            var base64Data = $"data:image/unknow;base64,{Convert.ToBase64String(data)}";
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => ImageUtils.ToLocalFile(base64Data));
+        }
+
+        [TestMethod()]
+        public void HttpsTest()
+        {
+            var file = ImageUtils.ToLocalFile("https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png");
+            Assert.IsTrue(file.EndsWith(".png"));
+            Assert.IsTrue(File.Exists(file));
+        }
+
+        public void HttpTest()
+        {
+            var file = ImageUtils.ToLocalFile("http://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png");
+            Assert.IsTrue(file.EndsWith(".png"));
+            Assert.IsTrue(File.Exists(file));
+        }
+    }
+}
