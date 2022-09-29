1ad0e24f7a79295dc39147caea143d32c3b11313
yanglb <me@yanglb.com>
2022-09-29 16:00:00 +0800

    添加CellUtils及其测试代码

--- TemplateGO/Utils/CellUtils.cs
+++ TemplateGO/Utils/CellUtils.cs
@@ -1,4 +1,5 @@
 ﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System.Text.RegularExpressions;
 
 namespace TemplateGO.Utils
 {
@@ -19,5 +20,43 @@ namespace TemplateGO.Utils
             }
             return value;
         }
+
+        /// <summary>
+        /// 获取行值，从1开始 如 A10 返回10, B5 返回 5
+        /// </summary>
+        public static int? RowValue(string? cellReference)
+        {
+            if (string.IsNullOrEmpty(cellReference)) return null;
+            string columnReference = Regex.Replace(cellReference.ToUpper(), @"[\d]", string.Empty);
+
+            int columnNumber = -1;
+            int mulitplier = 1;
+
+            //working from the end of the letters take the ASCII code less 64 (so A = 1, B =2...etc)
+            //then multiply that number by our multiplier (which starts at 1)
+            //multiply our multiplier by 26 as there are 26 letters
+            foreach (char c in columnReference.ToCharArray().Reverse())
+            {
+                columnNumber += mulitplier * ((int)c - 64);
+
+                mulitplier = mulitplier * 26;
+            }
+
+            //the result is zero based so return columnnumber + 1 for a 1 based answer
+            //this will match Excel's COLUMN function
+            return columnNumber + 1;
+        }
+
+        /// <summary>
+        /// 获取列值，从1开始 如 A10 返回1, B5 返回 2
+        /// </summary>
+        public static int? ColumnValue(string? cellReference)
+        {
+            if (string.IsNullOrEmpty(cellReference)) return null;
+            var match = Regex.Match(cellReference, @"(\d+)");
+            if (!match.Success) return null;
+            return int.Parse(match.Groups[1].Value);
+        }
+
     }
 }
---
+++ TemplateGOTests/Utils/CellUtilsTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TemplateGO.Utils.Tests
+{
+    [TestClass()]
+    public class CellUtilsTests
+    {
+        [DataRow(null, null)]
+        [DataRow("", null)]
+        [DataRow("A1", 1)]
+        [DataRow("B10", 2)]
+        [DataRow("Z2", 26)]
+        [DataRow("AA1", 27)]
+        [DataRow("AB1", 28)]
+        [DataRow("ZA2", 677)]
+        [TestMethod()]
+        public void RowValueTest(string? cell, int? column)
+        {
+            Assert.AreEqual(column, CellUtils.RowValue(cell));
+        }
+
+        [DataRow(null, null)]
+        [DataRow("", null)]
+        [DataRow("A1", 1)]
+        [DataRow("A10", 10)]
+        [DataRow("Z2", 2)]
+        [TestMethod()]
+        public void ColumnValueTest(string? cell, int? column)
+        {
+            Assert.AreEqual(column, CellUtils.ColumnValue(cell));
+        }
+    }
+}
