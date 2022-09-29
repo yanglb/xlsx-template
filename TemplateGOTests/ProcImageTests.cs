c7d8af15d4423b8eca693b2b5055af0ab20f040a
yanglb <me@yanglb.com>
2022-09-29 14:54:46 +0800

    添加ProcImage测试代码

--- TemplateGOTests/ProcImageTests.cs
+++ TemplateGOTests/ProcImageTests.cs
@@ -1,6 +1,8 @@
 ﻿using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Text.Json;
 using TemplateGOTests;
 
 namespace TemplateGO.Tests
@@ -8,10 +10,17 @@ namespace TemplateGO.Tests
     [TestClass()]
     public class ProcImageTests
     {
+        private static readonly Dictionary<string, string> TestData = new()
+        {
+            { "url", "https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png" },
+            { "file", R.FullPath("data/image.jpg") }
+        };
+
         [TestMethod()]
         public void ImageTest()
         {
-            TemplateGO.Render(R.FullPath("data/image.xlsx"), R.JsonFromFile("data/image.json"), "image-out.xlsx");
+            var json = JsonSerializer.Serialize(TestData);
+            TemplateGO.Render(R.FullPath("data/image.xlsx"), json, "image-out.xlsx");
 
             // 应该能打开文档
             using var doc = SpreadsheetDocument.Open("image-out.xlsx", false);
