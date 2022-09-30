8c8b930d25a2c40f2e842a6a901743880106de4d
yanglb <me@yanglb.com>
2022-09-30 10:01:33 +0800

    添加Image选项处理及其测试代码

---
+++ TemplateGO/Parser/ImageOptions.cs
@@ -0,0 +1,38 @@
+﻿using TemplateGO.Utils;
+
+namespace TemplateGO.Parser
+{
+    public class ImageOptions
+    {
+        public ImageOptions(Dictionary<string, string> options)
+        {
+            if (options.ContainsKey("padding"))
+            {
+                Padding = ParserUtils.ParseValueToEmu(options["padding"]);
+            }
+            if(options.ContainsKey("fw"))
+            {
+                FrameWidth = ParserUtils.ParseValueToEmu(options["fw"]);
+            }
+            if (options.ContainsKey("fh"))
+            {
+                FrameHeight = ParserUtils.ParseValueToEmu(options["fh"]);
+            }
+        }
+
+        /// <summary>
+        /// 内边距 key = fw, default = 0
+        /// </summary>
+        public long Padding { get; private set; }
+
+        /// <summary>
+        /// 外框宽度 key = fw
+        /// </summary>
+        public long? FrameWidth { get; private set; }
+
+        /// <summary>
+        /// 外框高度 key = fh
+        /// </summary>
+        public long? FrameHeight { get; private set; }
+    }
+}
---
+++ TemplateGOTests/Parser/ImageOptionsTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TemplateGO.Parser.Tests
+{
+    [TestClass()]
+    public class ImageOptionsTests
+    {
+        [DataRow("${t|image}", 0, null, null)]
+        [DataRow("${t|image:padding=1.6in}", (long)1463040, null, null)]
+        [DataRow("${t|image:fw=20px}", null, (long)190500, null)]
+        [DataRow("${t|image:fh=2.5cm}", null, null, (long)900000)]
+        [DataRow("${t|image:padding=1.6in,fw=20px,fh=2.5cm}", (long)1463040, (long)190500, (long)900000)]
+        [DataRow("${t|image:padding=1.6in,fw=20px,fh=2.5cm,haha=34,ttt=55}", (long)1463040, (long)190500, (long)900000)]
+        [TestMethod()]
+        public void ImageOptionsTest(string input, long padding, long? fw, long? fh)
+        {
+            var grammar = new Grammar(input);
+            var res = new ImageOptions(grammar.Options);
+            Assert.IsNotNull(res);
+            Assert.AreEqual(padding, res.Padding);
+            Assert.AreEqual(fw, res.FrameWidth);
+            Assert.AreEqual(fh, res.FrameHeight);
+        }
+    }
+}
