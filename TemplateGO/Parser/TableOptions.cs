df3c8b4dc6b577a849dd98f6a5ff1730bc299dba
yanglb <me@yanglb.com>
2022-10-01 09:03:40 +0800

    添加TableOptions及其测试代码

---
+++ TemplateGO/Parser/TableOptions.cs
@@ -0,0 +1,28 @@
+﻿namespace TemplateGO.Parser
+{
+    public class TableOptions
+    {
+        public TableOptions() { }
+        public TableOptions(Dictionary<string, string> options)
+        {
+            if (options.ContainsKey("titleCount"))
+            {
+                TitleCount = int.Parse(options["titleCount"]);
+            }
+            if(options.ContainsKey("keepExists"))
+            {
+                KeepExists = true;
+            }
+        }
+
+        /// <summary>
+        /// 标题行数目 key = titleCount, default = 1
+        /// </summary>
+        public long TitleCount { get; private set; } = 1;
+
+        /// <summary>
+        /// 是否清除原有数据 key = keepExists, default = false
+        /// </summary>
+        public bool KeepExists { get; private set; } = false;
+    }
+}
---
+++ TemplateGOTests/Parser/TableOptionsTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TemplateGO.Parser.Tests
+{
+    [TestClass()]
+    public class TableOptionsTests
+    {
+        [DataRow("${t|table}", 1, false)]
+        [DataRow("${t|table: titleCount=2}", 2, false)]
+        [DataRow("${t|table: keepExists}", 1, true)]
+        [DataRow("${t|table:keepExists=dd, titleCount=3}", 3, true)]
+        [TestMethod()]
+        public void TableOptionsTest(string input, int titlaCount, bool keepExists)
+        {
+            var grammar = new Grammar(input);
+            var res = new TableOptions(grammar.Options);
+            Assert.IsNotNull(res);
+            Assert.AreEqual(titlaCount, res.TitleCount);
+            Assert.AreEqual(keepExists, res.KeepExists);
+        }
+    }
+}
