6c59dd57e3a6eecdb9d4126b2b0eb87cbb2008e6
yanglb <me@yanglb.com>
2022-10-01 08:51:15 +0800

    添加表格测试数据

---
+++ TemplateGOTests/ProcTableTests.cs
@@ -0,0 +1,24 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TemplateGOTests;
+
+namespace TemplateGO.Tests
+{
+    [TestClass()]
+    public class ProcTableTests
+    {
+        [TestMethod()]
+        public void TableTest()
+        {
+            TemplateGO.Render(R.FullPath("data/table.xlsx"), R.JsonFromFile("data/table.json"), "table-out.xlsx");
+
+            // 应该能打开文档
+            using var doc = SpreadsheetDocument.Open("table-out.xlsx", false);
+            Assert.IsNotNull(doc);
+            Assert.IsNotNull(doc.WorkbookPart);
+            var sheets = doc.WorkbookPart.Workbook.Descendants<Sheet>();
+            Assert.IsNotNull(sheets);
+        }
+    }
+}
