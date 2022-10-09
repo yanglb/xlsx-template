0561e52165b1742e6b135f58dea9c5735e8067a0
yanglb <me@yanglb.com>
2022-10-09 11:55:51 +0800

    清理 TransformOptions 中无用的数据

--- TemplateGO/TransformOptions.cs
+++ TemplateGO/TransformOptions.cs
@@ -5,16 +5,6 @@ namespace TemplateGO
     public struct TransformOptions
     {
         /// <summary>
-        /// 属性名
-        /// </summary>
-        public string Property { get; set; }
-
-        /// <summary>
-        /// 处理程序，参见 ProcessorType
-        /// </summary>
-        public string Processor { get; set; }
-
-        /// <summary>
         /// 当前执行的转换器
         /// </summary>
         public string Transform { get; set; }
