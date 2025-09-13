9db24b0f1522bb63058d20afed858d025a2411ae
yanglb <me@yanglb.com>
2025-09-13 10:12:36 +0800

    将处理器缓存迁移到 Processor 中实现
    Sheet 及 Table 均使用

--- TemplateGO/Processor/Processor.cs
+++ TemplateGO/Processor/Processor.cs
@@ -1,4 +1,4 @@
-﻿namespace TemplateGO.Processor
+namespace TemplateGO.Processor
 {
     internal class Processor
     {
@@ -17,18 +17,36 @@
         /// <param name="procType">处理器种类</param>
         public static IProcessor? ProcessorByType(string procType)
         {
-            if (!ProcessorTypeMap.ContainsKey(procType))
+            if (!ProcessorTypeMap.TryGetValue(procType, out Type? type))
             {
                 Console.WriteLine($"未定义 {procType} 处理程序。");
                 return null;
             }
 
-            var type = ProcessorTypeMap[procType];
             if (Activator.CreateInstance(type) is not IProcessor ins)
             {
                 throw new Exception($"无法实例化用于处理 {procType} 的处理程序。");
             }
             return ins;
+        }
+
+        // 处理器缓存
+        private static readonly Dictionary<string, IProcessor?> ProcessorCache = [];
+
+        /// <summary>
+        /// 获取处理程序(支持缓存)
+        /// </summary>
+        /// <param name="type">处理器种类</param>
+        public static IProcessor? ProcessorByTypeWithCache(string type)
+        {
+            if (!ProcessorCache.TryGetValue(type, out IProcessor? value))
+            {
+                var processor = ProcessorByType(type);
+                value = processor;
+                ProcessorCache[type] = value;
+            }
+
+            return value;
         }
     }
 }
